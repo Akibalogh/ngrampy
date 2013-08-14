fafcf9751b0d333e217eb1e95d22893806b896ab
Akibalogh <akibalogh@gmail.com>
2013-08-14 12:58:41 -0500

    minor modifications

--- download.py
+++ download.py
@@ -16,24 +16,28 @@ for link in BeautifulSoup(response, parseOnlyThese=SoupStrainer('a')):
 	if link.has_key('href'):
 		url = link['href']
 		
-		# IF we match what we want:
+		# IF we match 1-3-grams with the selected dataset
+		#if re.search("[123]gram.+20120701", url):
 		if re.search("[12]gram.+20120701", url):
 			# Decode this
 			m = re.search(r"googlebooks-([\w\-]+)-(\d+)gram.+",url)
-			language, n = m.groups(None)
-			
+			language, n_gram = m.groups(None)
+	
 			# Only download some language
-			if language not in set(["eng-us-all","eng-gb-all", "fre-all", "ger-all", "heb-all", "ita-all", "rus-all", "spa-all" ]): continue
+			if language not in set(["eng-us-all"]): continue
+			#if language not in set(["eng-us-all","eng-gb-all", "fre-all", "ger-all", "heb-all", "ita-all", "rus-all", "spa-all" ]): continue
+
+			# Only download some n-grams
+			#if n_gram not in set(["1", "2", "3"]): continue
+			if n_gram not in set(["1", "2"]): continue
 
 			filename = re.split(r"/", url)[-1] # last item on filename split
 			
 			# Make the directory if it does not exist
 			if not os.path.exists(language):       os.mkdir(language)
-			if not os.path.exists(language+"/"+n): os.mkdir(language+"/"+n)
+			if not os.path.exists(language+"/"+n_gram): os.mkdir(language+"/"+n_gram)
 			
-			if not os.path.exists(language+"/"+n+"/"+filename):
-				print "# Downloading %s to %s" % (url, language+"/"+n+"/"+filename)
-				urllib.urlretrieve(url, language+"/"+n+"/"+filename )
-			
-        
-        
+			if not os.path.exists(language+"/"+n_gram+"/"+filename):
+				#pass
+				print "# Downloading %s to %s" % (url, language+"/"+n_gram+"/"+filename)
+				urllib.urlretrieve(url, language+"/"+n_gram+"/"+filename )
--- process-initial.sh
+++ process-initial.sh
@@ -2,6 +2,6 @@
 
 # A script to initially process google data. The significantly speeds up everything later.
 
-nohup pigz -dc /CorpusA/GoogleBooks/eng-gb-all/2/* | python process-google.py > /CorpusA/GoogleBooks/Processed/eng-gb-2 &
-nohup pigz -dc /CorpusA/GoogleBooks/eng-us-all/2/* | python process-google.py > /CorpusA/GoogleBooks/Processed/eng-us-2 &
-
+nohup pigz -dc /media/data0/ngrampy/eng-us-all/1/* | python process-google.py > /media/data0/ngrampy/Processed/eng-us-all-1 2> error-process-google.py && mailx -s "finished books processing" akibalogh@gmail.com < /dev/null &
+#nohup pigz -dc /media/data0/ngrampy/eng-us-all/test/* | python process-google.py > /media/data0/ngrampy/Processed/eng-us-all/test/eng-us-all-test &
+#nohup pigz -dc /CorpusA/GoogleBooks/eng-gb-all/2/* | python process-google.py > /CorpusA/GoogleBooks/Processed/eng-gb-2 &
