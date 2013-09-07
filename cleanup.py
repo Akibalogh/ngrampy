f7e0ab1de6b04cde4c7a9f1ea8bcb2b885e84006
Ning Ke <ningske@gmail.com>
2013-09-07 06:44:00 -0400

    Fix for n-grams and speed up extraction.
    - Fix so script work for general n-grams (n >= 1).
    - Speed up extraction by using one big regex, formed by several sub
      regexes.
    - Tried on google ngram set (1gram-"q", and 2-gram "hx").

--- cleanup.py
+++ cleanup.py
@@ -28,6 +28,122 @@
 
 import re
 import sys
+import functools
+
+def memo(f):
+    ''' Memoize function ''f''. '''
+    cache = {}
+    @functools.wraps(f)
+    def wrap(*args):
+        if args not in cache:
+            cache[args] = f(*args)
+        return cache[args]
+    return wrap
+
+# Only letters from A to Z, optionally, allow a single hyphen (-) or
+# an apostrophy (') in the middle (not at either end).
+# Also allow a single dot (.) in the front: ".NET", ".com"
+english_word_pattern = r"\.?[a-zA-Z]+(?:[-'][a-zA-Z]+)*"
+
+# Same as ''english_word_pattern'' but add abbreviations formed by replacing
+# part of a word with a single period (.). For example:
+# "U.S.A", "U.S.A.", "Mrs.", "Prof.". (The "." at the end seems optional)
+english_word_with_abbrev_pattern = r"[a-zA-Z]+(?:\.[a-zA-Z])*\.?"
+
+# Either plain word or with abbreviation
+english_and_abbrev_pattern = "(?:" + english_word_pattern + ")" + "|" \
+    + "(?:" + english_word_with_abbrev_pattern + ")"
+
+##
+# These compiled regex generator functions are memoized so it should be fast.
+#
+@memo
+def ngram_re(num, word_pattern):
+    ''' Get a compiled regex for ''num'' grams. It joins the
+    ''word_pattern'' for ''num'' times with a single space. '''
+    pat = "^" + ' '.join([word_pattern] * num) + "$"
+    return re.compile(pat)
+
+@memo
+def google_dictionary_pattern(word_pattern):
+    ''' Get a compiled regex for the google dictionary notation (dot notation)
+    using ''word_pattern'' for the word part. '''
+    dot_pattern = r"(?:\.\d+)?(?:_(?:NUM|VERB|DET|NOUN|ADP|X|ADJ|ADV|PRON|PRT|CONJ|\.))?"
+    # "'", "-" and "." cannot be included because they have special meanings.
+    punctuations = "[~!@#$%^&*()\]\[{}:\"<>?,/\\|=+0-9]*"
+    return punctuations + "(" + word_pattern + ")" + punctuations + dot_pattern
+
+def english_words_only(gram, year, count):
+    ''' Works with n-grams. '''
+    parts = re.split(r" +", gram)
+    compiled_pat = ngram_re(len(parts), google_dictionary_pattern(
+            english_and_abbrev_pattern))
+    matched = compiled_pat.match(gram)
+    if matched:
+        return (' '.join(matched.groups()), year, count)
+    else:
+        return (None, year, 0)
+
+def test_regex():
+    test_cases = [
+        ("Abundent", "Abundent"),
+        ("accent", "accent"),
+        ("wEiRD", "wEiRD"),
+        ("IMPORTANT", "IMPORTANT"),
+        ("Mrs.", "Mrs."),
+        ("Ph.D.", "Ph.D."),
+        ("U.S.A", "U.S.A"),
+        ("knock-down", "knock-down"),
+        ("slowly.5_ADV", "slowly"),
+        ("Question_NUM", "Question"),
+        ("Prof..5_NOUN", "Prof."),
+        ("M.D.12_X", "M.D."),
+        ("OK,", "OK"),
+        ("question5_NOUN", "question"),
+        ("Hi!.22_X", "Hi"),
+        ("$dollar", "dollar"),
+        ("#@%^tune()*&", "tune"),
+        ("?string++", "string"),
+        ("!string12", "string"),
+        ("!string4.5_VERB", "string"),
+        (".com_NOUN", ".com"),
+        (".com.5_X", ".com"),
+        ("good.2_ADJ", "good"),
+        # These don't work: Do we care?
+        # ".", "-", "'" are reserved (for google notation and abbreviations)
+        ("..vegan-.", None),
+        ("vegan_'", None),
+        ("'ve", None),
+        ("smooth.latte", None), # really a two gram
+        ]
+    for inp, expected in test_cases:
+        actual = english_words_only(inp, 2000, 1)[0]
+        if not actual == expected:
+            print "'%s': Fail: Expected: '%s' Actual '%s'" % (inp, expected, actual)
+    return
+
+## Filter by an external dictionary
+def dictionary_words_only(dictionary):
+    def dictionary_filter(gram, year, count):
+        ''' Filter out everything except words that are in a dictionary. '''
+        return (gram, year, count) \
+            if all([g in dictionary for g in gram.split(' ')]) \
+            else ('', year, 0)
+    return dictionary_filter
+ 
+conf_min_year = 1950
+conf_min_count = 10
+
+##
+# Order matters: For example, the minimus year function is the first because
+# it is fast and filters out a lot of things; the lower case conversion is last
+# because it is not needed unless the ngram is to be kept.
+#
+filters = [
+    lambda g, y, c: (g, y, 0 if y < conf_min_year else c), # minimum year
+    english_words_only,
+    lambda g, y, c: (g.lower(), y, c), # to lower case
+    ]
 
 def process_line(line, filters, results):
     """ Process a 'line' of ngram data, applying each filter in 'filters'
@@ -38,36 +154,13 @@ def process_line(line, filters, results):
     year = int(year)
     count = int(count)
     for f in filters:
+        if count == 0: # No need to apply subsequent filters
+            return
         gram, year, count = f(gram, year, count)
     if gram not in results:
         results[gram] = 0
     #sys.stderr.write("\t\t%s %d %d\n" % (gram, year, count))
     results[gram] += count
- 
-conf_min_year = 1950
-conf_min_count = 10
-
-part_of_speech_pattern = re.compile \
-    (r"^(\S+)(\.\d+)?_(num|verb|det|noun|adp|x|adj|adv|pron|prt|conj)$")
-
-google_dictionary_pattern = re.compile \
-    (r"^(\S+)(\.\d+)$")
-
-def remove_dot_notation(gram, year, count):
-    ''' Throw away dot notation. '''
-    match = part_of_speech_pattern.match(gram)
-    if match:
-        gram = match.group(1)
-    match = google_dictionary_pattern.match(gram)
-    if match:
-        gram = match.group(1)
-    return (gram, year, count)
-
-filters = [
-    lambda g, y, c: (g.lower(), y, c), # to lower case
-    lambda g, y, c: (g, y, 0 if y < conf_min_year else c), # minimum year
-    remove_dot_notation
-    ]
 
 def process_ngram_data(fin):
     """ Process data from file object ''fin''. """
@@ -79,4 +172,6 @@ def process_ngram_data(fin):
         if c >= conf_min_count:
             print "%s\t%d" % (g, c) 
 
-process_ngram_data(sys.stdin)
+if __name__ == "__main__":
+    #test_regex()
+    process_ngram_data(sys.stdin)
